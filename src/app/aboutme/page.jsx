c427e5e892554af424ca2b89d2c98a77aae1b1fc
Chris <55262451+ChristopherHoganJr@users.noreply.github.com>
2024-09-11 20:14:10 -0700

    added template pages for links

---
+++ src/app/aboutme/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main>
+      <section>
+        <h1>About Me Page</h1>
+      </section>
+    </main>
+  );
+};
+
+export default page;
---
+++ src/app/myprojects/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main>
+      <section>
+        <h1>My Projects Page</h1>
+      </section>
+    </main>
+  );
+};
+
+export default page;
